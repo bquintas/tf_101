c38cc10642358a969c5ec2c6d989cec8591c67ab
Bruno Quintas <bquintas@amazon.com>
2022-12-15 15:38:39 +0100

    Update aws-cfn-helper-script.py

--- aws-cfn-helper-script.py
+++ aws-cfn-helper-script.py
@@ -2,15 +2,17 @@ import sys
 from subprocess import call
 from subprocess import check_output
 
-if len(sys.argv) != 4:
-    print ("Error wrong parameter number")
-    print ("Usage: python aws-cfn-helper-script.py region prefix destroy")
-    print ("Example: python aws-cfn-helper-script.py eu-central-1 myprefix False")
-    sys.exit(1)
 
-myregion = sys.argv[1]
-prefix = sys.argv[2]
-destroy = sys.argv[3]
+
+# if len(sys.argv) != 4:
+#     print ("Error wrong parameter number")
+#     print ("Usage: python aws-cfn-helper-script.py region prefix destroy")
+#     print ("Example: python aws-cfn-helper-script.py eu-central-1 myprefix False")
+#     sys.exit(1)
+
+# myregion = sys.argv[1]
+# prefix = sys.argv[2]
+# destroy = sys.argv[3]
 
 mycommands = ["terraform"]
 
@@ -19,7 +21,7 @@ if destroy == "True":
 else:
     mycommands.extend(["apply"])
 
-mycommands.extend([ "-var", "region="+myregion , "-var", "prefix="+prefix])
+# mycommands.extend([ "-var", "region="+myregion , "-var", "prefix="+prefix])
 
 print (mycommands)
 call(mycommands)
